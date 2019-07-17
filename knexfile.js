50ea89ad65d0a7ea3e0368f9605d894a3b5fc106
kmcknight1 <katiemcknight1991@gmail.com>
2019-07-16 19:45:23 -0500

    knexfile edited

--- knexfile.js
+++ knexfile.js
@@ -7,5 +7,14 @@ module.exports = {
       filename: "./data/note-taker.db3"
     },
     useNullAsDefault: true
+  },
+
+  production: {
+    client: "pg",
+    debug: true,
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./migrations"
+    }
   }
 };
